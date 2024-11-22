b14887d5bbcd3ed4c1661c80f7af68fd9d3660ee
mohamad olleik <mhmd.olleik17@gmail.com>
2024-11-22 13:43:47 +0200

    c5

---
+++ index.js
@@ -0,0 +1,22 @@
+async function createStudent(){
+    const name=document.getElementById('name').value;
+    const age=document.getElementById('age').value;
+    const city=document.getElementById('city').value;
+
+
+    const response = await fetch(' https://threecloudsol.onrender.com/student', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Ensure the server expects JSON
+          "Authorization": "Bearer your-token" // Optional: Add if required by the API
+        },
+        body: JSON.stringify(name,age,city) // Convert JavaScript const to JSON string
+      });
+      
+      if(response.ok){
+        alert('student created succesfully')
+      }
+      else{
+        alert('failed to add student')
+      }
+}
